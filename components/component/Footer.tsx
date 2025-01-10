ca29d8061ed912c03b0551579c35427192938aa0
bsse24029 <bsse24029@itu.edu.pk>
2025-01-11 00:20:13 +0500

    first commit

---
+++ components/component/Footer.tsx
@@ -0,0 +1,13 @@
+// components/component/Footer.tsx
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full text-center py-4 bg-gray-800 text-white fixed bottom-0">
+      &copy; Website was made by Muhammad Hassan
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/component/Navbar.tsx
@@ -0,0 +1,31 @@
+import {
+  GitHubLogoIcon,
+  Link1Icon,
+  Link2Icon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className=" w-full bg-background max-sm:border-b max-sm:border-solid border-gray-900 flex px-4 justify-between items-center  h-[60px] md:h-[70px]">
+      <div className="flex items-center sm:hidden space-x-3">
+        <img src="/Logo.png" className="  w-6 h-6"></img>
+        <h1 className="text-lg font-medium">Gen AI</h1>
+      </div>
+      <div className=" ml-auto flex items-center gap-1 ">
+        <a href="mailto:hassan.uozair2022@gmail.com" className=" hover:bg-secondary/50 p-2 rounded-md cursor-pointer">
+          <Link1Icon className="w-5 h-5" />
+        </a>
+        <a href="https://www.linkedin.com/in/muhammad-h-a98975306/" className=" hover:bg-secondary/50 p-2 rounded-md cursor-pointer">
+          <LinkedInLogoIcon className="w-5 h-5" />
+        </a>
+        <a href="https://github.com/hassan252927" className=" hover:bg-secondary/50 p-2 rounded-md cursor-pointer">
+          <GitHubLogoIcon className="w-5 h-5" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
